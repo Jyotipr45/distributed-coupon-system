2ad5acf5eb431cf50839f80ae53a12e130d48f23
jyotipr45 <jyotipr45@gmail.com>
2026-08-12 01:29:58 +0530

    Project foundation and package structure

--- src/main/java/com/backendlab/coupon/DistributedCouponSystemApplication.java
+++ src/main/java/com/backendlab/coupon/DistributedCouponSystemApplication.java
@@ -1,4 +1,4 @@
-package com.backendlab.distributedsystem;
+package com.backendlab.coupon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
