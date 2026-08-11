1ac897a7b816a8317d281efd074f987374a131f3
jyotipr45 <jyotipr45@gmail.com>
2026-08-12 01:18:08 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/backendlab/distributedsystem/DistributedCouponSystemApplication.java
@@ -0,0 +1,13 @@
+package com.backendlab.distributedsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedCouponSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedCouponSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/backendlab/distributedsystem/DistributedCouponSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendlab.distributedsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedCouponSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
